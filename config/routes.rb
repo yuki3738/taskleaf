5f32d9ab8ecdb51f9e87fb0d50a6344e7c601f1e
Yuki Minamiya <y.minamiya@gmail.com>
2019-01-01 17:04:32 +0900

    Change routes.rb
    - Change the root page to task#index
    - Change syntax from get to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-  get 'tasks/edit'
+  root to: 'tasks#index'
+  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
