80b81422e5d48b1985ca26ddff60f4562c59ddae
yuki3738 <y.minamiya@gmail.com>
2019-01-20 17:36:12 +0900

    Add pagenate info

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,8 @@ class TasksController < ApplicationController
 
   def index
     @q = current_user.tasks.ransack(params[:q])
-    @tasks = @q.result(distinct: true).recent
+    @tasks = @q.result(distinct: true).page(params[:page])
+
 
     respond_to do |format|
       format.html
